66d82c70a5168eb7eab9e0ab1d0092a718fead3d
Jakob Givoni <jakob.givoni@vendoservices.com>
2013-08-14 09:15:41 +0200

    docs

--- _docs/EnvironmentConfig.php
+++ _docs/EnvironmentConfig.php
@@ -1,14 +1,26 @@
 <?php
 
-class EnvironmentConfig extends Replanner\DevelopmentConfig
+class EnvironmentConfig extends AppNamespace\DevelopmentConfig
 {
-	protected $baseUrl = "http://replanner.local/";
-	protected $databaseConnections = array(
-		'replanner' => array(
-			'host' => 'localhost',
-			'database' => 'replanner',
-			'user' => 'webapp',
-			'password' => 'BFvmny5awwFvbvRt',
-		),
-	);
+    /**
+     * Root url string
+     * This cannot be autodetected yet. Used for links and internal requests and redirects
+     * @var string
+     */
+    protected $baseUrl = "http://hostname/";
+
+    /**
+     * List of database connections
+     * - each named coonnection needs to specify host, db name, user and password
+     * @var array
+     */
+    protected $databaseConnections = array(
+        'replanner' => array(
+            'host' => 'localhost',
+            'database' => 'myapp',
+            'user' => 'user',
+            'password' => '',
+        ),
+    );
+
 }
